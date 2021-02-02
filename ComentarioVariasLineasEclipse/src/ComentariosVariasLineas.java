09c1044181d28b1d93661935f0f65bcb5a84fb2b
Smulli <78427713+Smulli@users.noreply.github.com>
2021-02-02 17:09:43 +0100

    Script de ComentariosVariasLineas de UD2 con Eclipse.

---
+++ ComentarioVariasLineasEclipse/src/ComentariosVariasLineas.java
@@ -0,0 +1,14 @@
+public class ComentariosVariasLineas{
+	
+	public static void main(String args[]){
+		/*System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("Cucu");*/
+		
+		for(int i=0;i<6; i++){
+			System.out.print(i+" ");
+			
+		}
+		System.out.println("FIN DEL PROGRAMA");
+	}
+}
